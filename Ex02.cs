3eb93cb89bb7f1ae1abcf72c07c8635ba77fc1e6
Ricardo Luiz <ricardo1204a@gmail.com>
2021-06-16 22:57:33 -0300

    Exercício 2

---
+++ Ex02.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ex02 {
+    class Program {
+        static void Main(string[] args) {
+
+            double salario;
+            double porcentagem;
+            double novoSalario;
+
+            Console.WriteLine("#####################################################");
+            Console.WriteLine("#                 Reajuste Salarial                 #");
+            Console.WriteLine("#####################################################");
+
+
+            Console.Write("Por favor, informe o salário base: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Agora informe o valor do reajuste: ");
+            porcentagem = Convert.ToDouble(Console.ReadLine());
+
+
+            novoSalario = Reajuste(salario, porcentagem);
+
+            Console.WriteLine("O seu novo salário com rejuste de " + porcentagem + "% é de: " + novoSalario);
+
+        }
+
+        static double Reajuste(double s, double p) {
+
+            double novoSal;
+
+            novoSal = s +((p * s)/100);
+            
+            
+            return novoSal;
+        }
+    }
+}
