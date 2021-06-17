1ef3fb945bc89396a59ea0a263840485b0af42c6
Ricardo Luiz <ricardo1204a@gmail.com>
2021-06-17 15:02:19 -0300

    Exercício 8

---
+++ Ex08.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exer08 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("######################################################");
+            Console.WriteLine("#          Ordenando números em um array             #");
+            Console.WriteLine("######################################################");
+
+            int[] array = new int[10];
+            int numero;
+
+            for (int i = 0; i < array.Length; i++) {
+                Console.WriteLine("Informe um número");
+               numero = Convert.ToInt32(Console.ReadLine());
+                array[i] = numero;  
+            }
+
+            Console.WriteLine("Array ordenado");
+
+            Array.Sort(array);
+
+            foreach (int value in array) Console.WriteLine("[" + value + "]");
+
+            Console.ReadLine();  
+        }
+
+    }
+}
+
