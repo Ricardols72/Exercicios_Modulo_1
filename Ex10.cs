2e381317cbc19ea7563a1595e6cf9513a87559a9
Ricardo Luiz <ricardo1204a@gmail.com>
2021-06-17 15:02:49 -0300

    Exercício 10

---
+++ Ex10.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exe10 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("###############################################");
+            Console.WriteLine("#         Verificação de triângulo            #");
+            Console.WriteLine("###############################################");
+
+
+
+            CalcTriangulo obj = new CalcTriangulo();
+
+
+            Console.Write("Informe o primeiro lado: ");
+            obj.a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o segundo lado: ");
+            obj.b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o terceiro lado: ");
+            obj.c = Convert.ToInt32(Console.ReadLine());
+
+            obj.Triangulo();
+        }
+    }
+}
+
+
+// Classe para cálculodo triângulo
+
+
+    class CalcTriangulo {
+
+        public int a;
+        public int b;
+        public int c;
+
+        public void Triangulo() {
+
+            if ((a < b + c) && (b < a + c) && (c < a + b)) {
+                Console.WriteLine("É um Triângulo");
+            } else {
+                Console.WriteLine("Não é um Triângulo");
+            }
+        }
+    }
+
+
